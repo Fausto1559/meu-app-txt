8409e10cf26dd325ece07f091bce80b3e32993b0
fausto <faustoefiscal@gmail.com>
2026-08-08 09:31:03 -0300

    fix: atualiza config com projeto novo firebase do zero

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -1,14 +1,20 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBp8LL3SXDRYUGIRTZ4QiX0ZYKhvL808",
+  apiKey: "AIzaSyBPsX2XBRavYr9Pq_e1lDQJc33B1pviDwo",
   authDomain: "meu-app-txt.firebaseapp.com",
   projectId: "meu-app-txt",
   storageBucket: "meu-app-txt.firebasestorage.app",
-  messagingSenderId: "510528690056",
-  appId: "1:510528690056:web:68a7b654a60659cea80c3d"
-};
+  messagingSenderId: "874084952005",
+  appId: "1:874084952005:web:0da0226bfda53965404dac",
+  };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const auth = getAuth(app);
