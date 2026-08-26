1dd19948116eab924d34a29254fa16f0a1ac887c
fausto <faustoefiscal@gmail.com>
2026-08-26 18:09:20 -0300

    Corrige exportacao do db no firebaseConfig

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -1,17 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"; // 1. Importar getFirestore
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBPsX2XBRavYr9Pq_e1lDQJc33B1pviDwo",
-  authDomain: "meu-app-txt.firebaseapp.com",
-  projectId: "meu-app-txt",
-  storageBucket: "meu-app-txt.firebasestorage.app",
-  messagingSenderId: "874084952005",
-  appId: "1:874084952005:web:0da0226bfda53965404dac"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
+export const db = getFirestore(app); // 2. Exportar a variável db
